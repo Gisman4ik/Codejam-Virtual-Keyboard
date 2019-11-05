43df4baaa84d811b0beb900678825c446dac216f
Gisman4ik <Sftawitkakpro@gmail.com>
2019-11-06 01:25:38 +0300

    refactor: fix all eslint airbnb-base problems

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function htmlStructureLoad () {
-  let keysValueRuLower = ['ё','1','2','3','4','5','6','7','8','9','0','-','=','Backspace','Tab','й','ц','у','к','е','н','г','ш','щ','з','х','ъ','Enter','Capslock','ф','ы','в','а','п','р','о','л','д','ж','э','Shift','я','ч','с','м','и','т','ь','б','ю','.','|','Shift','Ctrl','Win','Alt','Space','Alt','Cmd','Ctrl'];
-  let keysValueRuUpper = ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '-', '=','Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ','Enter', 'Capslock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.','|', 'Shift','Ctrl', 'Win', 'Alt','Space', 'Alt', 'Cmd', 'ctrl'];
-  let keysValueEngLower =['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=','Backspace','Tab','q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']','Enter','Capslock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '"','Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/','|','Shift','Ctrl','Win','Alt','Space','Alt','Cmd','Ctrl'];
-  let keysValueEngUpper = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+','Backspace','Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']','Enter','Capslock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"','Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '|','Shift', 'Ctrl','Win','Alt','Space','Alt','Cmd','Ctrl'];
+document.addEventListener('DOMContentLoaded', function htmlStructureLoad() {
+  let keysValueRuLower = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'Enter', 'Capslock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '|', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Cmd', 'Ctrl'];
+  let keysValueRuUpper = ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '-', '=', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', 'Enter', 'Capslock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', '|', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Cmd', 'ctrl'];
+  let keysValueEngLower = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'Enter', 'Capslock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '"', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '|', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Cmd', 'Ctrl'];
+  let keysValueEngUpper = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', 'Enter', 'Capslock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '|', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Cmd', 'Ctrl'];
   let keysEventValues = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Enter', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'Backslash', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ContextMenu', 'ControlRight'];
   const keys = [];
   const wrapper = document.createElement('div');
@@ -12,18 +12,18 @@ document.addEventListener('DOMContentLoaded', function htmlStructureLoad () {
     const keyBtn = document.createElement('div');
     keyBtn.classList.add(classKey);
     return keyBtn;
-  }
+  };
   const createSpan = (classSpan) => {
     const keySpan = document.createElement('span');
-    keySpan.classList.add (classSpan);
+    keySpan.classList.add(classSpan);
     return keySpan;
-  }
+  };
   const buttonsFill = () => {
-    for (let i = 0; i < 60; i++) {
+    for (let i = 0; i < 60; i += 1) {
       keys.push(createKeyBtn('keyBtn'));
-      keys[i].id=keysEventValues[i];
+      keys[i].id = keysEventValues[i];
     }
-  }
+  };
   const addButtonClasses = () => {
     wrapper.classList.add('wrapper');
     textarea.classList.add('textarea');
@@ -42,77 +42,118 @@ document.addEventListener('DOMContentLoaded', function htmlStructureLoad () {
     space.classList.add('keyBtn_space');
     const cmd = keys[58];
     cmd.classList.add('keyBtn_cmd');
-  }
+  };
   const createButtonsSpans = ()=>{
-    keys.forEach ((btn,counter) => {
+    let counter = 0;
+    keys.forEach((btn) => {
       keyboard.append(btn);
 
       btn.append(createSpan('value-ru'));
       btn.childNodes[0].classList.add('hide');
-        btn.childNodes[0].append (createSpan ('value-lower'));
-        btn.childNodes[0].childNodes[0].append (keysValueRuLower[counter]);
-        btn.childNodes[0].append (createSpan ('value-upper'));
-        btn.childNodes[0].childNodes[1].classList.add('hide');
-        btn.childNodes[0].childNodes[1].append (keysValueRuUpper[counter]);
+      btn.childNodes[0].append(createSpan('value-lower'));
+      btn.childNodes[0].childNodes[0].append(keysValueRuLower[counter]);
+      btn.childNodes[0].append(createSpan('value-upper'));
+      btn.childNodes[0].childNodes[1].classList.add('hide');
+      btn.childNodes[0].childNodes[1].append(keysValueRuUpper[counter]);
 
       btn.append(createSpan('value-eng'));
-        btn.childNodes[1].append (createSpan ('value-lower'));
-        btn.childNodes[1].childNodes[0].append (keysValueEngLower[counter]);
-        btn.childNodes[1].append (createSpan ('value-upper'));
-        btn.childNodes[1].childNodes[1].classList.add('hide');
-        btn.childNodes[1].childNodes[1].append (keysValueEngUpper[counter]);
+      btn.childNodes[1].append(createSpan('value-lower'));
+      btn.childNodes[1].childNodes[0].append(keysValueEngLower[counter]);
+      btn.childNodes[1].append(createSpan('value-upper'));
+      btn.childNodes[1].childNodes[1].classList.add('hide');
+      btn.childNodes[1].childNodes[1].append(keysValueEngUpper[counter]);
 
-      counter++;
-    })
-  }
+      counter += 1;
+    });
+  };
   buttonsFill();
   addButtonClasses();
   createButtonsSpans();
-  textarea.setAttribute('readonly','true');
-  document.body.append (wrapper);
-  document.querySelector('.wrapper').append (textarea,keyboard);
+  textarea.setAttribute('readonly', 'true');
+  document.body.append(wrapper);
+  document.querySelector('.wrapper').append(textarea, keyboard);
 });
-document.addEventListener('DOMContentLoaded', function keyboradFeatures () {
+document.addEventListener('DOMContentLoaded', function keyboradFeatures() {
   let language = 'ENG';
   let symbolCase = 'Lower';
   let keyBtnsRU = document.querySelectorAll('.value-ru');
   let keyBtnsENG = document.querySelectorAll('.value-eng');
-  const textarea = document.querySelector('.textarea');
   let keyBtns = document.querySelectorAll('.keyBtn');
   const capslockEvent = ()=>{
-    if (symbolCase === "Lower") {
-      symbolCase = "Upper";
+    if (symbolCase === 'Lower') {
+      symbolCase = 'Upper';
       keyBtnsRU.forEach((node)=> {
         node.childNodes[0].classList.add('hide');
         node.childNodes[1].classList.remove('hide');
-      })
+      });
       keyBtnsENG.forEach((node)=> {
         node.childNodes[0].classList.add('hide');
         node.childNodes[1].classList.remove('hide');
-      })
-      return
-    }
-    else {
-      symbolCase = "Lower";
+      });
+    } else {
+      symbolCase = 'Lower';
       keyBtnsRU.forEach((node)=> {
         node.childNodes[1].classList.add('hide');
         node.childNodes[0].classList.remove('hide');
-      })
+      });
       keyBtnsENG.forEach((node)=> {
         node.childNodes[1].classList.add('hide');
         node.childNodes[0].classList.remove('hide');
-      })
-      return
+      });
     }
-  }
+  };
+  const eventOnPress = (textarea, button) => {
+    if (button.id === 'CapsLock') {
+      capslockEvent();
+      return undefined;
+    }
+    if (button.id === 'Space') {
+      return `${textarea.value} `;
+    }
+    if (button.id === 'Enter') {
+      return `${textarea.value}\n`;
+    }
+    if (button.id === 'Backspace') {
+      const size = textarea.value.length;
+      if (size > 1) return textarea.value.slice(0, size - 1);
+      return undefined;
+    }
+    if (button.id === 'Tab' || button.id === 'ControlLeft' || button.id === 'AltLeft'
+      || button.id === 'ShiftLeft' || button.id === 'MetaLeft' || button.id === 'ShiftRight'
+      || button.id === 'AltRight' || button.id === 'ContextMenu' || button.id === 'ControlRight') {
+      return undefined;
+    }
+    if (language === 'ENG') {
+      if (symbolCase === 'Lower') {
+        let symbol = button.childNodes[1].childNodes[0].innerText;
+        return `${textarea.value}${symbol}`;
+      }
+      if (symbolCase === 'Upper') {
+        let symbol = button.childNodes[1].childNodes[1].innerText;
+        return `${textarea.value}${symbol}`;
+      }
+    }
+    if (language === 'RU') {
+      if (symbolCase === 'Lower') {
+        let symbol = button.childNodes[0].childNodes[0].innerText;
+        return `${textarea.value}${symbol}`;
+      }
+      if (symbolCase === 'Upper') {
+        let symbol = button.childNodes[0].childNodes[1].innerText;
+        return `${textarea.value}${symbol}`;
+      }
+    }
+    return undefined;
+  };
   const keyHandler = (event) => {
-    let button,result;
+    const textarea = document.querySelector('.textarea');
+    let button;
+    let result;
     if (event.code) {
       button = document.getElementById(`${event.code}`);
+    } else {
+      button = event.target.closest('.keyBtn');
     }
-    else {
-      button = event.target.closest('.keyBtn')
-    };
 
     if (event.type === 'keydown') {
       button.classList.add('active-key');
@@ -120,111 +161,59 @@ document.addEventListener('DOMContentLoaded', function keyboradFeatures () {
       if (result !== undefined || result != null) {
         textarea.value = result;
       }
-      return;
-    }
-    else if (event.type === 'keyup') {
+    } else if (event.type === 'keyup') {
       button.classList.remove('active-key');
-      return;
-    }
-    else if (event.type === 'mousedown') {
+    } else if (event.type === 'mousedown') {
       button.classList.add('active-key');
       result = eventOnPress(textarea, button);
       if (result !== undefined || result != null) {
         textarea.value = result;
       }
-      return;
-    }
-    else if (event.type === 'mouseout') {
+    } else if (event.type === 'mouseout') {
       button.classList.remove('active-key');
-      return;
-    }
-    else if (event.type === 'mouseup') {
+    } else if (event.type === 'mouseup') {
       button.classList.remove('active-key');
-      return;
+    } else {
+      return undefined;
     }
-    else {return};
-  }
-  const eventOnPress = (textarea, button) => {
-    if (button.id === 'CapsLock') {
-      capslockEvent();
-      return;
-    }
-
-    if (button.id ==='Space') {
-      return `${textarea.value} `;
-    }
-    if (button.id === 'Enter') {
-      return `${textarea.value}\n`;
-    }
-    if (button.id ==='Backspace') {
-      const size = textarea.value.length;
-      if (size > 1) return textarea.value.slice(0, size - 1);
-      return '';
-    }
-    if(button.id ==='Tab' || button.id ==='ControlLeft' || button.id ==='AltLeft'
-      ||button.id ==='ShiftLeft' || button.id ==='MetaLeft' || button.id ==='ShiftRight'
-      || button.id === 'AltRight' || button.id === 'ContextMenu'|| button.id ==='ControlRight') {
-      return;
-    }
-    if(language === "ENG") {
-      if(symbolCase === "Lower"){
-      let symbol = button.childNodes[1].childNodes[0].innerText;
-      return `${textarea.value}${symbol}`;
-      }
-      else if (symbolCase === "Upper"){
-        let symbol = button.childNodes[1].childNodes[1].innerText;
-        return `${textarea.value}${symbol}`;
-      }
-    }
-    if(language === "RU") {
-      if(symbolCase === "Lower"){
-      let symbol = button.childNodes[0].childNodes[0].innerText;
-      return `${textarea.value}${symbol}`;
-      }
-      else if (symbolCase === "Upper"){
-        let symbol = button.childNodes[0].childNodes[1].innerText;
-        return `${textarea.value}${symbol}`;
-      }
-    }
-  }
+    return undefined;
+  };
   const shiftChange = (event) => {
-    if(event.code === 'ShiftLeft' || event.code === 'ShiftRight'){
-      if(event.type === 'keydown'){
+    if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+      if (event.type === 'keydown') {
         capslockEvent();
       }
       if (event.type === 'keyup') {
         capslockEvent();
       }
     }
-  }
+  };
   const languageChange = (event) => {
-    if (event.altKey && event.ctrlKey){
+    if (event.altKey && event.ctrlKey) {
       if (language === 'RU') {
         language = 'ENG';
         keyBtnsRU.forEach((node) => {
           node.classList.add('hide');
-        })
+        });
         keyBtnsENG.forEach((node) => {
           node.classList.remove('hide');
-        })
-      }
-      else {
-        language='RU';
+        });
+      } else {
+        language = 'RU';
         keyBtnsENG.forEach((node) => {
           node.classList.add('hide');
-        })
+        });
         keyBtnsRU.forEach((node) => {
           node.classList.remove('hide');
-        })
+        });
       }
     }
-  }
-
+  };
   keyBtns.forEach((key)=>{
     key.addEventListener('mousedown', keyHandler);
     key.addEventListener('mouseup', keyHandler);
     key.addEventListener('mouseout', keyHandler);
-  })
+  });
   document.addEventListener('keydown', shiftChange);
   document.addEventListener('keyup', shiftChange);
   document.addEventListener('keydown', languageChange);
